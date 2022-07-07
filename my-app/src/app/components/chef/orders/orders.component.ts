496d27870962c13d4c1e9ef196953d3bb8ed0b1a
Areliz Oyarce <arelizuwc@gmail.com>
2022-07-06 20:52:55 -0500

    Show order in chef-view Closed #25

--- my-app/src/app/components/chef/orders/orders.component.ts
+++ my-app/src/app/components/chef/orders/orders.component.ts
@@ -9,21 +9,13 @@ import { FirestoreService } from 'src/app/services/services-firestore/firestore.
 })
 export class OrdersComponent implements OnInit {
   orders: sendOrderList[] = []
-  oneSummaryOrder: any[] = []
   constructor(private firestore: FirestoreService) { }
 
   ngOnInit(): void {
-    console.log(this.printOrder())
-  }
-  printOrder() {
     this.firestore.getOrder().subscribe((order) => {
       console.log('soy order', order)
       this.orders = order
-      order.forEach((ordenes) => {
-        this.oneSummaryOrder.push(ordenes.order)
-      })
     })
-    return this.oneSummaryOrder
   }
 
   // changeStatusOrder(timepara: number){
